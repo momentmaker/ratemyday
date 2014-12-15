68b98bf4524bd838563155e3c96c8871a44c512e
Frank Zhu <frankyufengzhu@gmail.com>
2014-12-14 21:59:22 -0500

    Took out time validation

--- app/models/day.rb
+++ app/models/day.rb
@@ -4,5 +4,4 @@ class Day < ActiveRecord::Base
   validates :rating, presence: true
 
   validates_date :date, is_at: Date.current
-  validates_time :date, on_or_after: '8:00pm', on_or_after_message: 'must be at the end of your day'
 end
